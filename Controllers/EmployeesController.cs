48d0fa37f56bfd9d9a96d9e3576c2a8f11fa21f3
A'lo <57810226+Saidalokhon@users.noreply.github.com>
2021-11-04 03:27:34 +0500

    init: created initial application

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using DSCC.CW1._7902.UI.Models;
+
+namespace DSCC.CW1._7902.UI.Controllers
+{
+    // Create EmployeesController class, that extends BaseController. As a
+    // generic parameters Employee model is passed
+    public class EmployeesController : BaseController<Employee>
+    {
+        // Passing "employees" as modelName parameter
+        public EmployeesController(string modelName = "employees") : base(modelName)
+        {
+        }
+    }
+}
