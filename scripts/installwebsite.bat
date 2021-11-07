f51894305c3f222eee9164d5c79acb79d7cd807e
A'lo <57810226+Saidalokhon@users.noreply.github.com>
2021-11-07 13:57:42 +0500

    fix: removed redundant directions from bat scripts

--- scripts/installwebsite.bat
+++ scripts/installwebsite.bat
@@ -3,4 +3,4 @@
 IF NOT EXIST c:\DSCC.CW1.7902.UI mkdir c:\DSCC.CW1.7902.UI
 cd c:DSCC.CW1.7902.UI\scripts
 
-IF EXIST c:\DSCC.CW1.7902.UI\scripts\installwebsite.ps1 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
+IF EXIST c:\DSCC.CW1.7902.UI\scripts\installwebsite.ps1 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "installwebsite.ps1"
