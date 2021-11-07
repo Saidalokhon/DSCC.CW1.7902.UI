13fbba920f01a950fb85b03ab27aa54373567829
A'lo <57810226+Saidalokhon@users.noreply.github.com>
2021-11-07 12:27:22 +0500

    init: initial deploy

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DSCC.CW1._7902.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Models/IModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DSCC.CW1._7902.UI.Models
-{
-    // Create interface for models.
-    public interface IModel
-    {
-        // Common variable of all models is Id.
-        public int Id { get; set; }
-    }
-}
