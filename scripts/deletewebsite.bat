ebac4337c51f6d69cb4f0f9a977d351a8ca3ccde
A'lo <57810226+Saidalokhon@users.noreply.github.com>
2021-11-07 14:03:54 +0500

    fix: added site autostart

--- scripts/deletewebsite.bat
+++ scripts/deletewebsite.bat
@@ -3,7 +3,7 @@
 
 IF NOT EXIST c:\DSCC.CW1.7902.UI\scripts mkdir c:\DSCC.CW1.7902.UI\scripts
 cd c:\DSCC.CW1.7902.UI\scripts
-IF EXIST c:\DSCC.CW1.7902.UI\scripts\deletewebsite.ps1 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "deletewebsite.ps1"
+IF EXIST c:\DSCC.CW1.7902.UI\scripts\deletewebsite.ps1 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "c:\DSCC.CW1.7902.UI\scripts\deletewebsite.ps1"
 
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "rm c:\DSCC.CW1.7902.UI -Recurse -Force"
 del /q "c:\DSCC.CW1.7902.UI\*.*"
